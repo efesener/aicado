1ca0baf07e38e7845cc4429aca7cc36b709b2819
Efe Şener <78027374+efesener@users.noreply.github.com>
2025-05-12 13:38:13 +0300

    Fix a bug
    A bug fixed causes block set height function for some iframes.

--- aicado.js
+++ aicado.js
@@ -1,13 +1,21 @@
 window.addEventListener('message', function (event) {
 
     if (event.origin === "https://run.aicado.ai" && event.data.action === "aicadoResize" && event.data.frameHeight > 0) {
+
+
         var iframes = document.getElementsByTagName('iframe');
         for (var i = 0; i < iframes.length; i++) {
+
+
             if (!iframes[i].src) continue; // Skip iframe if src is missing
+
 
             try {
                 var iframeSrc = new URL(iframes[i].src);
+
+
             } catch (e) {
+
                 continue; // Skip iframe if URL is invalid
             }
 
@@ -17,9 +25,13 @@ window.addEventListener('message', function (event) {
             var audiValue = searchParams.get('audi');
             var slValue = iframes[i].src.split('go.aicado.ai/')[1]?.split('?')[0];
 
+            
+
             if ((slValue === event.data.slParameter) || (sValue === event.data.sParameter && tValue === event.data.tParameter) || (audiValue === event.data.audiParameter) ) {
+                
+                
                 iframes[i].style.height = event.data.frameHeight + 'px';
-                break;
+                // break;
             }
         }
     }
@@ -47,7 +59,7 @@ window.addEventListener('message', function (event) {
             if ((slValue === event.data.slParameter) || (sValue === event.data.sParameter && tValue === event.data.tParameter) || (audiValue === event.data.audiParameter) ) {
                 var iframeRect = iframes[i].getBoundingClientRect();
                 totalOffset = iframeRect.top + event.data.top + window.pageYOffset;
-                break;
+                // break;
             }
         }
 
